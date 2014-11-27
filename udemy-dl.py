04d2998215b94c7f857a96ba40cb479209677b09
Gaganpreet <gaganpreet.arora@gmail.com>
2014-11-27 23:37:53 +0530

    Add a helpful message for people looking for old script

---
+++ udemy-dl.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+print('''udemy-dl can now be installed using pip
+
+pip install udemy-dl
+
+
+If you are looking for the old script, it is now located here:
+
+src/udemy_dl/udemy_dl.py
+''')
